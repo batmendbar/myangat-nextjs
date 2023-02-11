8e442d06605c44f1aa8fe091cd033503efe22c65
Batmend Batsaikhan <batmendbar@gmail.com>
2023-02-10 23:09:25 -0600

    Adds boiler plate

---
+++ app/head.jsx
@@ -0,0 +1,16 @@
+export default function Head() {
+	return (
+		<>
+			<title>Мянгат</title>
+			<meta
+				content="width=device-width, initial-scale=1"
+				name="viewport"
+			/>
+			<meta
+				name="description"
+				content="Mongolian science competition results"
+			/>
+			<link rel="icon" href="/favicon.png" />
+		</>
+	);
+}
