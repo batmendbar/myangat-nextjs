8c80de15b2db3a29110652c37bca7a397a91d940
Batmend Batsaikhan <batmendbar@gmail.com>
2023-02-12 23:51:14 -0600

    Adds hall of fame

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,16 @@
+import Link from "next/link";
+
 export default function Home() {
-	return <main></main>;
+	return (
+		<main className="homepage">
+			<div>Мянгат - Монголын олимпиадын нэгдсэн сан. </div>
+			<div>
+				Мэдээллийн залруулга болон өөрчлөлтийн талаар
+				<Link href="mailto:batmend@gmail.com">
+					batmend&#64;gmail.com
+				</Link>
+				руу мэйл илгээнэ үү.
+			</div>
+		</main>
+	);
 }
